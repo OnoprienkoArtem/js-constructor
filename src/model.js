9e84b6cdad36ca25a3eda3f181c7618fc4a65ac0
OnoprienkoArtem <o.artem.e@gmail.com>
2020-08-30 13:19:06 +0300

    create classes for different templates

--- src/model.js
+++ src/model.js
@@ -1,35 +1,35 @@
 import image from './assets/javascript.jpeg';
+import {Block, TitleBlock} from './classes/blocks';
 
 export const model = [
-    {
-        type: 'title',        
-        value: 'Test title',
-        options: {
+    new TitleBlock(        
+        'Test title',
+        {
             tag: 'h2',
             styles: 'background: silver; color: #fff; text-align: center; padding: 1.5rem;'
-        },
-    },
-    {
-        type: 'image',
-        value: image,
-        options: {
+        }
+    ),
+    new ImageBlock(
+        'image',
+        image,
+        {
             styles: 'padding: 2rem 0; display: flex; justify-content: center;',
             alt: 'my image',
             imageStyles: 'width: 500px; height: auto'
         }
-    },
-    {
-        type: 'text',
-        value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-        options: {
+    ),
+    new TextBlock(
+        'text',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        {
             styles: 'background: tomato; color: black;'
-        },
-    },
-    {
-        type: 'textColumns',
-        value: ['1. Lorem ipsum dolor sit amet', '2. Lorem ipsum dolor sit amet', '3. Lorem ipsum dolor sit amet'],
-        options: {
+        }
+    ),
+    new TextColumnsBlock(
+        'textColumns',
+        ['1. Lorem ipsum dolor sit amet', '2. Lorem ipsum dolor sit amet', '3. Lorem ipsum dolor sit amet'],
+        {
             styles: 'padding: 2rem 0; color: gray;'
-        },
-    }
+        }
+    )
 ];
